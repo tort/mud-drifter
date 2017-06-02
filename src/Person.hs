b2fb14a7c271d8de90aca13382ca2c13195e332d
Alexey Nesterov <alexey.nesterov@cinarra.com>
2017-06-01 21:00:05 -0700

    add simple event network

--- src/Person.hs
+++ src/Person.hs
@@ -2,10 +2,27 @@
 
 module Person (
   personHandle
+  , personBot
 ) where
 
 import Data.Text
+import Reactive.Banana
+import Reactive.Banana.Frameworks
+
+data GoCommand = GoCommand
 
 personHandle :: Text -> Text
 personHandle ":connect" = ":connected"
 personHandle text = text
+
+personBot :: IO ()
+personBot = do 
+    (addGoCommandHandler, fireGoCommand) <- newAddHandler
+    network <- compile $ personNetworkDesc addGoCommandHandler
+    actuate network
+    fireGoCommand GoCommand
+
+personNetworkDesc :: AddHandler GoCommand -> MomentIO ()
+personNetworkDesc addGoCommandHandler = do
+    goCommandEvent <- fromAddHandler addGoCommandHandler
+    reactimate $ putStrLn "go event fired" <$ goCommandEvent
