a36dd187ca740eee49003a5a8c12f05dd7ee0536
Alexey Nesterov <alexey.nesterov@cinarra.com>
2017-06-02 15:16:38 -0700

    simple walker

--- src/Person.hs
+++ src/Person.hs
@@ -8,8 +8,13 @@ module Person (
 import Data.Text
 import Reactive.Banana
 import Reactive.Banana.Frameworks
+import Data.Maybe
 
-data GoCommand = GoCommand
+newtype GoCommand = GoCommand [String]
+data StopCommand = StopCommand
+newtype EnterLocationEvent = EnterLocationEvent Int
+data PulseEvent = PulseEvent
+newtype GoDirectionAction = GoDirectionAction String
 
 personHandle :: Text -> Text
 personHandle ":connect" = ":connected"
@@ -18,11 +23,54 @@ personHandle text = text
 personBot :: IO ()
 personBot = do 
     (addGoCommandHandler, fireGoCommand) <- newAddHandler
-    network <- compile $ personNetworkDesc addGoCommandHandler
-    actuate network
-    fireGoCommand GoCommand
+    (addStopCommandHandler, fireStopCommand) <- newAddHandler
+    (addEnterLocationHandler, fireEnterLocationEvent) <- newAddHandler
+    (addPulseEvent, firePulseEvent) <- newAddHandler
 
-personNetworkDesc :: AddHandler GoCommand -> MomentIO ()
-personNetworkDesc addGoCommandHandler = do
-    goCommandEvent <- fromAddHandler addGoCommandHandler
-    reactimate $ putStrLn "go event fired" <$ goCommandEvent
+    network <- compile $ do
+        goCommandEvent <- fromAddHandler addGoCommandHandler
+        stopCommandEvent <- fromAddHandler addStopCommandHandler
+        enterLocationEvent <- fromAddHandler addEnterLocationHandler
+        pulseEvent <- fromAddHandler addPulseEvent
+
+        ifGo <- accumB False $ unions $ [(\x -> True) <$ goCommandEvent, (\x -> False) <$ stopCommandEvent]
+        path <- accumB Nothing $ unions $ [(\(GoCommand path) acc -> Just path) <$> goCommandEvent, removePathHead <$ enterLocationEvent]
+        let filteredPulseEvent = whenE ifGo pulseEvent
+        let pathEvent = path <@ whenE (bPathNotEmpty path) filteredPulseEvent 
+        let goDirectionAction = (\p -> GoDirectionAction $ Prelude.head $ fromJust p) <$> pathEvent
+
+        reactimate $ putStrLn "pulse event" <$ pulseEvent
+        reactimate $ printDirection <$> goDirectionAction
+
+    actuate network
+    firePulseEvent PulseEvent
+    fireGoCommand $ GoCommand ["north", "south", "west", "east"]
+    firePulseEvent PulseEvent
+    fireEnterLocationEvent $ EnterLocationEvent 1
+    firePulseEvent PulseEvent
+    fireEnterLocationEvent $ EnterLocationEvent 1
+    firePulseEvent PulseEvent
+    fireEnterLocationEvent $ EnterLocationEvent 1
+    firePulseEvent PulseEvent
+    fireEnterLocationEvent $ EnterLocationEvent 1
+    firePulseEvent PulseEvent
+    firePulseEvent PulseEvent
+    fireStopCommand StopCommand
+    firePulseEvent PulseEvent
+    firePulseEvent PulseEvent
+
+printDirection :: GoDirectionAction -> IO ()
+printDirection (GoDirectionAction direction) = putStrLn direction
+
+removePathHead :: Maybe [String] -> Maybe [String]
+removePathHead Nothing = Nothing
+removePathHead (Just []) = Nothing
+removePathHead x@(Just xs) = fmap Prelude.tail x
+
+bPathNotEmpty :: Behavior (Maybe [String]) -> Behavior Bool
+bPathNotEmpty = fmap pathNotEmpty
+
+pathNotEmpty :: Maybe [String] -> Bool
+pathNotEmpty Nothing = False
+pathNotEmpty (Just []) = False
+pathNotEmpty (Just xs) = True
