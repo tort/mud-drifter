c64aba7f46a45aada937fcdb609984ee6908acee
Alexey Nesterov <alexey.nesterov@cinarra.com>
2018-02-07 15:46:38 -0800

    read evt binary log

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Logger ( runLogger ) where
+module Logger ( runLogger
+              , readLog
+              ) where
 
 import Pipes
 import Pipes.Prelude
@@ -8,6 +10,7 @@ import Pipes.Concurrent
 import qualified Pipes.Prelude as PP
 import qualified Pipes.ByteString as PBS
 import Event
+import qualified Event as E
 import System.IO
 import qualified System.IO as IO
 import Prelude
@@ -17,7 +20,9 @@ import Control.Exception.Safe
 import Control.Monad
 import Data.Maybe
 import Data.Binary
-import Data.ByteString.Lazy (toStrict)
+import Data.Binary.Get
+import Data.ByteString.Lazy
+import qualified Data.ByteString.Lazy as BSL
 
 runLogger :: Input Event -> IO ()
 runLogger evtBusInput = do async $ (bracketWithError
@@ -26,3 +31,11 @@ runLogger evtBusInput = do async $ (bracketWithError
                                                 IO.hClose h)
                                     (\h -> runEffect $ fromInput evtBusInput >-> PP.map (toStrict . encode) >-> PBS.toHandle h >> liftIO (IO.putStr "logger input stream ceased")))
                            return ()
+
+readLog :: String -> IO [E.Event]
+readLog filePath = do contents <- BSL.readFile filePath
+                      return $ parse contents
+                        where parse "" = []
+                              parse input = let (Right (rem, _, evt)) = decodeEvent input
+                                             in evt : parse rem
+                                            where decodeEvent input = decodeOrFail input :: Either (BSL.ByteString, ByteOffset, String) (BSL.ByteString, ByteOffset, E.Event)
