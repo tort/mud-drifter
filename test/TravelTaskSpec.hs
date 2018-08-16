49383dc6fc6f0d1976833045407dad5405e1deb7
Alexey Nesterov <alexey.nesterov@cinarra.com>
2018-08-15 19:20:32 -0700

    extract Travel module

--- test/TravelTaskSpec.hs
+++ test/TravelTaskSpec.hs
@@ -19,6 +19,7 @@ import Control.Wire.Core
 import Event
 import qualified Event as E
 import Control.Arrow
+import Travel
 
 spec :: Spec
 spec = describe "travel task" $ do
@@ -51,26 +52,6 @@ spec = describe "travel task" $ do
         unknownEvent = ServerInput "unknown event"
     in do outEvts <- pushToWire (toEvtList <<< travelTo <<< newEvent) (return . Just <$> inEvts)
           outEvts `shouldBe` [TravelRequest path, locationEvent, ServerCommand "command", PulseEvent, PulseEvent, unknownEvent, ServerCommand "command"]
-
-travelTo :: Monad m => Wire m (W.Event E.Event) (W.Event E.Event)
-travelTo = proc inEvt -> do
-  bPath <- hold' [] -< unwrapPath <$> filterE isTravelRequest inEvt
-  bLoc <- hold' offlineLocationId -< unwrapLocId <$> filterE isLocation inEvt
-  arr (snd <$>) <<< scanE (False, PulseEvent) -< throttle . issueCmd bPath <$> inEvt
-    where isTravelRequest (TravelRequest _) = True
-          isTravelRequest _ = False
-          isLocation (ServerEvent (LocationEvent _ _)) = True
-          isLocation _ = False
-          unwrapPath (TravelRequest path) = path
-          unwrapLocId (ServerEvent (LocationEvent loc _)) = locId loc
-          offlineLocationId = 0 :: LocId
-          issueCmd path PulseEvent = case path of x:y:xs -> ServerCommand "command"
-                                                  _ -> PulseEvent
-          issueCmd _ e = e
-          throttle e@(ServerCommand _) = \acc -> case acc of (True, _) -> (True, PulseEvent)
-                                                             (False, _) -> (True, e)
-          throttle e@(ServerInput _) = const (False, e)
-          throttle e = \(flag, _) -> (flag, e)
 
 pushToWire :: Monad m => Wire m a [b] -> [a] -> m [b]
 pushToWire wire inEvts = fst <$> foldl runWire initAcc inEvts
